b92e5636b4fdf2e39a93f5cd9bff65d1814baef1
Heloisa <heloisa.f.santos10@aluno.senai.br>
2026-09-02 15:30:08 -0300

    alterações diversas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 
 from routes.pessoa_route import router as pessoa_router
 
@@ -9,15 +10,12 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
     allow_credentials=True,
-    allow_methods = ["*"],
+    allow_methods=["*"],
     allow_headers=["*"],
 )
 
+@app.get("/")
+def home():
+    return RedirectResponse(url="/docs")
+
 app.include_router(pessoa_router)
-
-"""
-    PARA EXECUTAR O PROJETO DIGITE A LINHA DE COMANDO:
-    uvicorn main:app --reload
-
-    python -m  uvicorn main:app --reload
-"""
--- models/pessoa_model.py
+++ models/pessoa_model.py
@@ -2,22 +2,19 @@ from sqlalchemy import Column, Integer, String, DECIMAL, Date
 
 from database import Base
 
+
 class Pessoa(Base):
     __tablename__ = "pessoa"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String(60))
-    cpf = Column(Integer)
-    data_nascimento = Column(Date)
-    peso = Column(Integer)
-    altura = Column(DECIMAL(10,2))
-    sexo = Column(String(1))
-    cep = Column(Integer)
-    rua_logradouro = Column(String(100))
-    bairro = Column(String(20))
-    cidade = Column(String(70))
-    uf = Column(String(2))
-    
-    
-   
 
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    nome = Column(String(60), nullable=True)
+    cpf = Column(Integer, nullable=True)
+    data_nascimento = Column(Date, nullable=True)
+    peso = Column(Integer, nullable=True)
+    altura = Column(DECIMAL(10, 2), nullable=True)
+    sexo = Column(String(1), nullable=True)
+    cep = Column(Integer, nullable=True)
+    rua_logradouro = Column(String(100), nullable=True)
+    bairro = Column(String(20), nullable=True)
+    cidade = Column(String(70), nullable=True)
+    uf = Column(String(2), nullable=True)
