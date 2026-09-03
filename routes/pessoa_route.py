395f087d4ddd954bf4492ff220f27b6efddbb866
Heloisa <heloisa.f.santos10@aluno.senai.br>
2026-09-03 14:12:21 -0300

    alterações diversas

--- routes/pessoa_route.py
+++ routes/pessoa_route.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+
+from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from database import SessionLocal
@@ -6,54 +7,56 @@ from database import SessionLocal
 from controllers.pessoa_controller import PessoaController
 from schemas.pessoa_schema import PessoaSchema
 
+
 router = APIRouter(
     prefix="/pessoa",
     tags=["Pessoa"]
 )
+
 controller = PessoaController()
+
 
 def get_db():
     db = SessionLocal()
-    
+
     try:
         yield db
     finally:
         db.close()
 
-@router.get("/")
-def listar():
-    db = next(get_db())
 
+@router.get("/")
+def listar(db: Session = Depends(get_db)):
     return controller.listar(db)
 
-@router.get("/{id}")
-def listar_id(id: int):
-    db = next(get_db())
 
+@router.get("/{id}")
+def listar_id(id: int, db: Session = Depends(get_db)):
     return controller.listar_id(db, id)
 
+
 @router.post("/")
-def cadstrar(pessoa: PessoaSchema):
-    db = next(get_db())
+def cadastrar(pessoa: PessoaSchema, db: Session = Depends(get_db)):
     return controller.cadastrar(db, pessoa)
 
+
 @router.put("/{id}")
-def alterar(id: int, pessoa: PessoaSchema):
-
-    db = next(get_db())
-
+def alterar(
+    id: int,
+    pessoa: PessoaSchema,
+    db: Session = Depends(get_db)
+):
     return controller.alterar(
         db,
         id,
         pessoa
     )
 
+
 @router.delete("/{id}")
-def excluir(id: int):
-
-    db = next(get_db())
-
+def excluir(id: int, db: Session = Depends(get_db)):
     return controller.excluir(
         db,
         id
     )
+
